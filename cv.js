6d2ee01352f7be87d34d071fca1a6ef0403c6d8c
Martina Latessa <martinalatessa@hotmail.com>
2023-09-30 01:23:54 -0300

    mi primer proyecto en github

---
+++ cv.js
@@ -0,0 +1,33 @@
+
+
+document.getElementById('boton_color').addEventListener('click', function(){
+    document.getElementById('column_side').style.backgroundColor = '#3c3c3c';
+    document.getElementById('column_side').style.color = 'white';
+
+    document.getElementById('column_side2').style.backgroundColor = '#3c3c3c';
+    document.getElementById('column_side2').style.color = 'white';
+
+    document.body.style.backgroundColor = '#252525';
+    document.body.style.color = 'white' ;
+
+    
+})
+
+
+document.getElementById('boton_default').addEventListener('click', function(){
+    document.getElementById('column_side').style.backgroundColor = '#333';
+    document.getElementById('column_side').style.color = 'white';
+
+    document.getElementById('column_side2').style.backgroundColor = '#333';
+    document.getElementById('column_side2').style.color = 'white';
+
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';}) 
+
+   
+
+
+
+
+  
+
